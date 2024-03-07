2f9ded6272c535a6004c0dafe53acc73007de522
Nada-Khater <nadakhater21@gmail.com>
2024-03-07 17:30:21 +0200

    add (create table) and change the name of (validate_dbname function)

--- dbms.sh
+++ dbms.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
+
 create_db() {
     read -p "Enter Database Name: " dbname
-    if $( validate_dbname "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]] 
+    if $( validate_name "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]] 
     then
         if [[ -e $dbname ]]
         then
             echo "Error! Duplicated Database Name, Database Already Exists!"
         else
             mkdir "./$dbname"
-            #cd $dbname 
             echo "$dbname Database Created Successfully"
         fi
     else
@@ -23,7 +23,7 @@ list_db() {
 
 connect_db() {
     read -p "Enter Database Name: " dbname
-    if $( validate_dbname "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]]
+    if $( validate_name "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]] 
     then
         if [[ -e $dbname  && -d $dbname ]]
         then
@@ -41,7 +41,7 @@ connect_db() {
 
 drop_db(){
     read -p "Enter Database Name: " dbname
-    if $( validate_dbname "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]]
+    if $( validate_name "$dbname" ) && ! [[ "$dbname" =~ [[:space:]] ]]
     then
         if [[ -e $dbname && -d $dbname ]]
         then
@@ -56,24 +56,23 @@ drop_db(){
 }
 
 # Ensures that the dbname is non zero length and not null
-validate_dbname() {
+validate_name() {
   [[ -n "$1" && "$1" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
 }
 
 
-main_menu()
-{
-select choice in "Create Database" "List Databases" "Connect To Databases" "Drop Database" "Exit"; 
-do
-    case $REPLY in
-        1) create_db ;;
-        2) list_db ;;
-        3) connect_db ;;
-        4) drop_db ;;
-        5) echo "Exiting..."; break ;;
-        *) echo "Invalid Choice! Choose option between 1 to 5." ;;
-    esac
-done
+main_menu(){
+    select choice in "Create Database" "List Databases" "Connect To Databases" "Drop Database" "Exit"; 
+    do
+        case $REPLY in
+            1) create_db ;;
+            2) list_db ;;
+            3) connect_db ;;
+            4) drop_db ;;
+            5) echo "Exiting..."; break ;;
+            *) echo "Invalid Choice! Choose option between 1 to 5." ;;
+        esac
+    done
 }
 
 main_menu
--- tabel.sh
+++ tabel.sh
@@ -1,59 +1,148 @@
 #!/bin/sh
-tabel_menu()
-{
-select choice in "Create table" "List table" "Select From Table" "Drop Table" "Delete From Table" "Update Table" "Insert Into Table" "Back To Main Menu" ;  
-do
-    case $REPLY in
-        1) create_table ;;
-        2) list_table ;;
-        3) select_table ;;
-        4) drop_table ;;
-        5) delete_table;;
-        6) update_table;;
-        7) insert_into_table;;
-        8) back_to_menu;;
-        *) echo "Invalid Choice! Choose option between 1 to 5." ;;
-    esac
-done
 
+tabel_menu(){
+    select choice in "Create table" "List table" "Select From Table" "Drop Table" "Delete From Table" "Update Table" "Insert Into Table" "Back To Main Menu" ;  
+    do
+        case $REPLY in
+            1) create_table ;;
+            2) list_table ;;
+            3) select_table ;;
+            4) drop_table ;;
+            5) delete_table ;;
+            6) update_table ;;
+            7) insert_into_table ;;
+            8) back_to_menu ;;
+            *) echo "Invalid Choice! Choose option between 1 to 8." ;;
+        esac
+    done
 }
 
-create_table(){
-         
-         touch file
+create_table() {
+    while true 
+    do
+        read -p "Enter Table Name: " tbname
+        if validate_name "$tbname" && ! [[ "$tbname" =~ [[:space:]] ]] 
+        then
+            if [[ -e "./$dbname/$tbname" ]] 
+            then
+                echo "Error! Duplicated Table Name, Table Already Exists!"
+            else
+                touch "./$dbname/$tbname"
+                touch "./$dbname/metadata_$tbname"
+                
+                while true 
+                do
+                    read -p "Enter number of fields: " num_of_fields
+                    if [[ $num_of_fields =~ ^[0-9]+$ ]] 
+                    then
+                        break
+                    else
+                        echo "Invalid input. Please enter an integer."
+                    fi
+                done
 
-        echo "implement here"
+                declare -a colnames=()
+                declare -a coltypes=()
+                for ((i=0; i<$num_of_fields; i++))
+                do
+                    while true 
+                    do
+                        read -p "Enter Column Name: " colname
+                        if ! validate_name "$colname" || [[ "$colname" =~ [[:space:]] ]] 
+                        then
+                            echo "Invalid Column Name, Enter a Valid Name."
+                        elif [[ " ${colnames[@]} " =~ " ${colname} " ]]
+                        then
+                            echo "Column Name '$colname' already exists. Enter a different name."
+                        else
+                            colnames+=("$colname")
+                            break
+                        fi
+                    done
+
+                    while true 
+                    do
+                        read -p "Enter Column Type (str/int): " coltype
+                        case $coltype in
+                            "str" | "int")
+                                coltypes+=("$coltype")
+                                break
+                                ;;
+                            *)
+                                echo "Invalid column type. Enter 'str' or 'int'"
+                                ;;
+                        esac
+                    done
+                done
+
+                echo "Choose One Primary Key from the above List:"
+                for col in "${colnames[@]}"
+                do
+                    echo "- $col"
+                done
+
+                while true 
+                do
+                    read -p "Enter Primary Key Name: " pk
+                    if [[ " ${colnames[@]} " =~ " ${pk} " ]] 
+                    then
+                        break
+                    else
+                        echo "Invalid Input. Please choose a field from the list."
+                    fi
+                done
+
+                for ((i=0; i<$num_of_fields; i++))
+                do
+                    metadata="${colnames[$i]}:${coltypes[$i]}"
+                    # Set primary key flag based on user input
+                    if [[ "${colnames[$i]}" == "${pk}" ]]
+                    then
+                        metadata+=":1"
+                    else
+                        metadata+=":0"
+                    fi
+
+                    echo "$metadata" >> "./$dbname/metadata_$tbname"
+                done
+
+                echo "Table '$tbname' Created Successfully."
+                break
+            fi
+        else
+            echo "Invalid Table Name, Enter a Valid Name."
+        fi
+    done
 }
 
 list_table(){
-
-        echo "implement here"
+    echo "implement here"
 }
+
 select_table(){
-
-        echo "implement here"
+    echo "implement here"
 }
+
 drop_table(){
-
-        echo "implement here"
+    echo "implement here"
 }
+
 delete_table(){
-
-        echo "implement here"
+    echo "implement here"
 }
+
 update_table(){
+    echo "$dbname"
+}
 
-        echo "$dbname"
+insert_into_table(){
+    echo "implement here"
 }
-insert_into_table()
-{
-     echo "implement here"
-}
-back_to_menu()
-{
-   cd /home/heba/DBMS-Bash
-   ./dbms.sh main_menu
-   exit
+
+back_to_menu(){
+    cd /home/heba/DBMS-Bash
+    ./dbms.sh main_menu
+    exit
 }
 
 tabel_menu
