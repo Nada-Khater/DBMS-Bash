bfbb012ccd51302d25d0d767c8a01a7786182289
Nada-Khater <nadakhater21@gmail.com>
2024-03-10 00:58:41 +0200

    add delete_from_table function

--- tabel.sh
+++ tabel.sh
@@ -8,7 +8,7 @@ tabel_menu(){
             2) list_table ;;
             3) select_table ;;
             4) drop_table ;;
-            5) delete_table ;;
+            5) delete_from_table ;;
             6) update_table ;;
             7) insert_into_table ;;
             8) back_to_menu ;;
@@ -129,7 +129,7 @@ drop_table() {
         fi
         
         echo "Tables in the database:"
-        ls ./$dbname | grep -v "^metadata_"
+        list_table # call list_table function
         
         read -p "Enter name of table to drop: " tbname
         case $tbname in
@@ -151,71 +151,196 @@ drop_table() {
         esac
     done
 }
-insert_into_table() {
-  echo "Available tables in $dbname"
-  list_table 
-  read -p "Enter table Name: " tbname
-  echo "Selected table: $tbname"
-
-  if [[ -f ./$dbname/$tbname ]]
-  then
-    record_values=()
-    columns=($(cut -d ':' -f 1 ./$dbname/metadata_$tbname))
-    constraints=($(cut -d ':' -f 2 ./$dbname/metadata_$tbname))
-    primary_keys=($(cut -d ':' -f 3 ./$dbname/metadata_$tbname))
-
-    for (( i=0; i<${#columns[@]}; i++ ))
-    do
-      field="${columns[$i]}"
-      while true
-      do
-        read -p "Enter $field Value: " value
-
-        value=${value% }
-        if [[ "${primary_keys[$i]}" == "1" ]]
-        then
-          if grep -qw "$value" ./$dbname/$tbname
-          then
-            echo "Primary key value already exists. Please enter a unique value."
-            continue
-          fi
-        fi
-
-        if [[ "${constraints[$i]}" == "int" ]]
-        then
-          if ! [[ "$value" =~ ^[0-9]+$ ]]
-          then
-            echo "Enter a valid number"
-            continue
-          fi
-
-        elif [[ "${constraints[$i]}" == "str" ]]
-        then
-          if ! validate_name "$value" || [[ "$value" =~ [[:space:]] ]]
-          then
-            echo "Enter a valid string, shouldn't contain space"
-            continue
-          fi
-        fi
-        break
-      done
-
-      record_values+=":$value"
-    done
-
-    echo ${record_values:1} >> ./$dbname/$tbname
-    echo "Data Entered Successfully"
-  else
-    echo "Data file does not exist or is not accessible."
-  fi
-}
-
 
 select_table(){
     echo "implement here"
 }
 
-delete_table(){
+
+update_table(){
+    echo "$dbname"
+}
+
+delete_from_table(){
+    while true
+    do
+        if [ -z "$(ls -A "./$dbname")" ]
+        then
+            echo "No tables found in the database."
+            break
+        fi
+        
+        echo "Tables in the database:"
+        list_table # call list_table function
+        
+        read -p "Enter name of table to delete from: " tbname
+        case $tbname in
+            *[!\ \/\\\*\-\+\#\$\%\^\&\*0-9]*) 
+                if [[ -f "./$dbname/$tbname" ]]
+                then
+                    echo "Choose Delete option:"
+                    select opt in "Delete All" "Delete Row"
+                    do
+                        case $opt in
+                            "Delete All")
+                                if [ "$(wc -l < "./$dbname/$tbname")" -gt 0 ]
+                                then
+                                    > "./$dbname/$tbname"
+                                    echo "All data deleted from table '$tbname'."
+                                else
+                                    echo "Table '$tbname' is already empty."
+                                fi
+                                break
+                                ;;
+
+                            "Delete Row")
+                                echo "Available columns in table '$tbname':"
+                                awk -F: '{print NR ". " $1}' "./$dbname/metadata_$tbname"
+
+                                while true
+                                do
+                                    read -p "Enter column name: " colname
+                                    if ! validate_name "$colname" || [[ "$colname" =~ [[:space:]] ]] 
+                                    then
+                                        echo "Invalid Column Name, Enter a Valid Name."
+                                        continue
+                                    fi
+                                    
+                                    # Check if the column exists in the metadata
+                                    if ! grep -q "^$colname:" "./$dbname/metadata_$tbname"
+                                    then
+                                        echo "Column '$colname' does not exist in table '$tbname'."
+                                        continue
+                                    fi
+                                    
+                                    # Get the type of the column from the metadata file
+                                    coltype=$(awk -F: -v colname="$colname" '$1 == colname {print $2}' "./$dbname/metadata_$tbname")
+                                    break
+                                done
+                                
+                                while true
+                                do
+                                    read -p "Enter value of $colname to delete: " value
+
+                                    # Check if the value exists in the table
+                                    if ! grep -q "\<$value\>" "./$dbname/$tbname"
+                                    then
+                                        echo "Value '$value' not found in table '$tbname'."
+                                        continue
+                                    fi
+
+                                    # Check if the value matches the column type
+                                    case $coltype in
+                                        int)
+                                            if [[ ! "$value" =~ ^[0-9]+$ ]]
+                                            then
+                                                echo "Invalid value. Expected an integer."
+                                                continue
+                                            fi
+                                            ;;
+                                        str)
+                                            if ! validate_name "$value" || [[ "$value" =~ [[:space:]] ]]
+                                            then
+                                                echo "Invalid value. Enter a valid string."
+                                                continue
+                                            fi
+                                            ;;
+                                        *)
+                                            echo "Unknown column type '$coltype'."
+                                            continue
+                                            ;;
+                                    esac
+                                    break
+                                done
+
+                                # Delete rows based on specified field and value
+                                columns=$(awk -F ':' '{print $1}' "./$dbname/metadata_$tbname")
+                                index=$(echo "$columns" | grep -wn "$colname" | cut -d: -f1)
+
+                                awk -F ':' -v col="$index" -v val="$value" '
+                                    BEGIN { OFS=FS }
+                                    { if ($col != val) print $0 } ' "./$dbname/$tbname" > "./$dbname/$tbname.tmp" && mv "./$dbname/$tbname.tmp" "./$dbname/$tbname"
+                                echo "Rows with '$colname = $value' deleted from table '$tbname'."
+                                break
+                                ;;
+                            *)
+                                echo "Invalid choice. Choose 1 or 2."
+                                ;;
+                        esac
+                    done
+                    break
+                else
+                    echo "Table '$tbname' not found! Enter a valid table name."
+                fi
+                ;;
+            *)
+                echo "Invalid input! Enter a valid table name."
+                ;;
+        esac
+    done
+}
+
+insert_into_table() {
+    echo "Available tables in $dbname"
+    list_table # call list_table function
+    read -p "Enter table Name: " tbname
+    echo "Selected table: $tbname"  
+    if [[ -f ./$dbname/$tbname ]]; then
+        record_values=()
+        columns=($(cut -d ':' -f 1 ./$dbname/metadata_$tbname))
+        constraints=($(cut -d ':' -f 2 ./$dbname/metadata_$tbname))
+        primary_keys=($(cut -d ':' -f 3 ./$dbname/metadata_$tbname))
+
+        for ((i=0; i<${#columns[@]}; i++))
+        do
+            field="${columns[$i]}"
+            while true
+            do
+                read -p "Enter $field Value: " value
+                
+                # Check if it's a primary key
+                if [[ "${primary_keys[$i]}" == "1" ]]
+                then
+                    if  [ $(grep -c "$value:" ./$dbname/$tbname) -gt 0 ]
+                    then
+                        echo "Primary key value already exists. Please enter a unique value."
+                        continue
+                    fi
+                fi
+                
+                # Check constraints
+                if [[ "${constraints[$i]}" == "int" ]]
+                then 
+                    if ! [[ "$value" =~ ^[0-9]+$ ]]
+                    then
+                        echo "Enter a valid number"
+                        continue
+                    fi
+                    
+                elif [[ "${constraints[$i]}" == "str" ]]
+                then 
+                    if ! validate_name "$value" || [[ "$value" =~ [[:space:]] ]]
+                    then
+                        echo "Enter a valid string, shouldn't contain space"
+                        continue
+                    fi
+                fi
+                
+                # If all checks pass, break out of the loop
+                break
+            done
+            
+            record_values+=":$value"
+        done  
+        echo ${record_values:1} >> ./$dbname/$tbname
+        echo "Data Entered Successfully"
+    else
+        echo "Data file does not exist or is not accessible."
+    fi
+}
+
+
+select_table(){
     echo "implement here"
 }
 
@@ -224,9 +349,7 @@ update_table(){
 }
 
 
-
 back_to_menu(){
-
     ./dbms.sh main_menu
     exit
 }
